a765bc3900c4937dee852f32049d2ebb3adde1a2
unknown <rebeca.costache@yahoo.com>
2024-10-09 12:57:40 +0300

    added script for changing state based on proximity

--- MyFirstAvatar/Assets/Resources/DistanceTracker.cs
+++ MyFirstAvatar/Assets/Resources/DistanceTracker.cs
@@ -1,30 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class DistanceTracker : MonoBehaviour
 {
-    public GameObject imageTarget1;
-    public GameObject imageTarget2;
+    public GameObject fox1;
+    public GameObject fox2;
+    public float attackDistance = 0.25f;
+    public ARTrackedImage trackedImage1; 
+    public ARTrackedImage trackedImage2;
+    
+    private Animator animator1;
+    private Animator animator2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator1 = fox1.GetComponent<Animator>();
+        animator2 = fox2.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(imageTarget1.transform.position, imageTarget2.transform.position);
-        Debug.Log("Distance: " + distance);
-        
-        if (distance < 1.0f)
+        if (trackedImage1.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking &&
+            trackedImage2.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking)
         {
-            // Acțiune la apropiere
+            float distance = Vector3.Distance(fox1.transform.position, fox2.transform.position);
+
+            if (distance < attackDistance)
+            {
+                EnterAttackState();
+            }
         }
-        else
-        {
-            // Acțiune la depărtare
-        }
+    }
+
+    private void EnterAttackState()
+    {
+        // Play the attack animation
+        animator1.SetTrigger("AttackTrigger");
+        animator2.SetTrigger("AttackTrigger");
+        Debug.Log("Objects are in attack range and attacking!");
     }
 }
